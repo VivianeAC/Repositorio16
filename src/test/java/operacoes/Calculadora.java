171463980a7b3b4dc4fff5324eedc66dec1bcfa2
Viviane <vivialmeidadecarvalho18@gmail.com>
2023-11-07 21:26:41 -0300

    subindoaula

---
+++ src/test/java/operacoes/Calculadora.java
@@ -0,0 +1,25 @@
+package operacoes;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		operacoesMatematica op  =  new operacoesMatematica () ;
+		
+		op.soma(5,20);
+		op.soma(100,352 );
+		
+		op.subtração(25,10);
+		op.subtração(423, 200);
+		
+		op.divisão(938, 12);
+		op.divisão(50,3);
+		
+		op.Multiplicação(264, 21);
+		op.Multiplicação(40,4);
+		
+	    
+	
+	}
+
+}
---
+++ src/test/java/operacoes/operacoesMatematica.java
@@ -0,0 +1,41 @@
+package operacoes;
+
+public class operacoesMatematica {
+
+	public  void soma (int n1, int n2) {
+		
+		int soma =  n1 + n2 ;
+		System.out.println("Soma :" + soma) ;
+	}
+		public  void subtração (int n1 , int n2) {
+			
+		int subtração =  n1 - n2 ;		
+		System.out.println("Subtração :"   + subtração );
+		}
+		
+		public  void divisão (int n1, int n2) {
+			
+		int divisão =  n1 / n2 ;
+		System.out.println("Divisão :"     +  divisão );
+		}
+		public  void Multiplicação (int n1, int n2) {
+		int Multiplicação  =  n1 * n2 ;
+		System.out.println("Multiplicação:" + Multiplicação );
+		
+		
+		}
+		
+		// Adição/Soma    +
+		// Subtração      -
+		// Divisão        /
+		// Multiplicação  *
+		
+	
+
+	
+	}
+
+
+	
+
+
